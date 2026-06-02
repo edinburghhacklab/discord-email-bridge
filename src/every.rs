999dc4aa4f07813c8c19a083536f92e4109344fe
Aria <me@aria.rip>
2026-06-02 14:16:47 +0100

    prune deps, remove unused

--- src/every.rs
+++ src/every.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 
-use tokio::{sync::oneshot, time};
+use tokio::time;
 use tokio_stream::{StreamExt, wrappers::IntervalStream};
 
 /// Every `duration`, call `func`. Return when `func` errors.
@@ -15,28 +15,4 @@ where
     }
 
     Ok(())
-}
-
-/// Every `duration`, call `func`. Return when `func` errors, or a message is sent to `shutdown`
-pub async fn every_until<Func, Fut, E>(
-    duration: Duration,
-    mut func: Func,
-    mut shutdown: oneshot::Receiver<()>,
-) -> Result<(), E>
-where
-    Func: FnMut() -> Fut,
-    Fut: Future<Output = Result<(), E>>,
-{
-    let mut stream = IntervalStream::new(time::interval(duration));
-
-    loop {
-        tokio::select! {
-            Some(_ts) = stream.next() => {
-                func().await?;
-            }
-            _ = &mut shutdown => {
-                return Ok(());
-            }
-        }
-    }
 }
